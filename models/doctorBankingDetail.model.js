6d313e543d55c014099fa3426a5510c40f9201c8
Mohit Garg <tl.webcodeft@gmail.com>
2024-09-23 05:55:21 -0700

    created controller for getBankingDetail by userID which has to match with the login user

--- models/doctorBankingDetail.model.js
+++ models/doctorBankingDetail.model.js
@@ -2,57 +2,64 @@ import mongoose from "mongoose";
 
 const doctorBankingDetail = mongoose.Schema(
   {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "DoctorSignup", // Assuming you have a User model
+      required: true,
+    },
     accountName: {
-        type: String,
-        required: [true, "Please enter your account name"],
-      },
-      bankName: {
-        type: String,
-        required: [true, "Please enter your bank name"],
-      },
-      bankAddress: {
-        type: String,
-        required: [true, "Please enter your bank address"],
-      },
-      bankAccountNumber: {
-        type: String,
-        // required: [true, "Please enter your bank account number"],
-        // minlength: [8, "Bank account number must be at least 8 characters"],
-      },
-      branchCodeIFSC: {
-        type: String,
-        required: [true, "Please enter your branch code or IFSC"],
-      },
-      bankAccountNumberIBAN: {
-        type: String,
-        required: [true, "Please enter your IBAN"],
-      },
-      fullName: {
-        type: String,
-        required: [true, "Please enter your full name"],
-      },
-      speciality: {
-        type: String,
-        required: [true, "Please enter your speciality"],
-      },
-      description: {
-        type: String,
-        required: [true, "Please provide a description"],
-      },
-      experience: {
-        type: String,
-        required: [true, "Please enter your years of experience"],
-      },
-      consultationCharges: {
-        type: String,
-        required: [true, "Please enter your consultation charges"],
-      }
+      type: String,
+      required: [true, "Please enter your account name"],
+    },
+    bankName: {
+      type: String,
+      required: [true, "Please enter your bank name"],
+    },
+    bankAddress: {
+      type: String,
+      required: [true, "Please enter your bank address"],
+    },
+    bankAccountNumber: {
+      type: String,
+      // required: [true, "Please enter your bank account number"],
+      // minlength: [8, "Bank account number must be at least 8 characters"],
+    },
+    branchCodeIFSC: {
+      type: String,
+      required: [true, "Please enter your branch code or IFSC"],
+    },
+    bankAccountNumberIBAN: {
+      type: String,
+      required: [true, "Please enter your IBAN"],
+    },
+    fullName: {
+      type: String,
+      required: [true, "Please enter your full name"],
+    },
+    speciality: {
+      type: String,
+      required: [true, "Please enter your speciality"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide a description"],
+    },
+    experience: {
+      type: String,
+      required: [true, "Please enter your years of experience"],
+    },
+    consultationCharges: {
+      type: String,
+      required: [true, "Please enter your consultation charges"],
+    },
   },
   {
     timestamps: true,
   }
 );
 
-
-const DoctorBankingDetail = mongoose.model("doctorBankingDetail", doctorBankingDetail);
+const DoctorBankingDetail = mongoose.model(
+  "doctorBankingDetail",
+  doctorBankingDetail
+);
 export default DoctorBankingDetail;
--- routes/doctorBankingDetail.route.js
+++ routes/doctorBankingDetail.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {bankingDetailRegistration, getbankingDetails, getbankingDetail, updatebankingDetail, deletebankingDetail} from "../controllers/doctorBankingDetail.controller.js";
+import {bankingDetailRegistration, getbankingDetails, getbankingDetail, getBankingDetailByUserId,updatebankingDetail, deletebankingDetail} from "../controllers/doctorBankingDetail.controller.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -7,8 +7,10 @@ const router = express.Router();
 router.post("/",authenticateToken, bankingDetailRegistration);
 router.get("/", getbankingDetails);
 router.get("/:id", getbankingDetail);
+router.get("/user/:userId", getBankingDetailByUserId);
 router.put("/:id",authenticateToken, updatebankingDetail);
-router.delete("/:id",authenticateToken, deletebankingDetail);
+router.delete("/:id", deletebankingDetail);
 
 
 export default router;
+  
