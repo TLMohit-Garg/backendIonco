156adeef03b1636ceb20299d46d7bfa53a81d4bd
Mohit Garg <tl.webcodeft@gmail.com>
2024-10-19 12:11:43 +0530

    Develop api for the videoLink consultation

--- models/videoLinkConsultationDetails.js
+++ models/videoLinkConsultationDetails.js
@@ -1,12 +1,21 @@
 import mongoose from "mongoose";
 
 const VideocallConsultationSchema = new mongoose.Schema({
-    patientId: { type: String, required: [true, "Please enter patientId"] },
-    doctorId: { type: String, required: [true, "Please enter doctorId"] },
-    consultationTime: { type: Date, required: [true, "Please enter consultationTime"] },
-    videoCallLink: { type: String },
-    createdAt: { type: Date, default: Date.now }
-  });
+  patientId: { type: String, required: [true, "Please enter patientId"] },
+  doctorId: { type: String, required: [true, "Please enter doctorId"] },
+  consultationTime: {
+    type: Date,
+    required: [true, "Please enter consultationTime"],
+  },
+  videoCallLink: { type: String },
+  startTime: { type: Date, default: Date.now },
+  endTime: { type: Date },
+  notes: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
 
-   const VideocallConsultation = mongoose.model('VideoConsultation', VideocallConsultationSchema);
-   export default VideocallConsultation;
+const VideocallConsultation = mongoose.model(
+  "VideoConsultation",
+  VideocallConsultationSchema
+);
+export default VideocallConsultation;
